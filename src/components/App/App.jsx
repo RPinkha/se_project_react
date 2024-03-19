0a35c8a06b888eab41766969acb776682397d858
RuvenPinkhasov <rubi00010@gmail.com>
2024-03-18 23:22:52 -0400

    finished cards outline and padding of the entire page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
+
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import "./App.css";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "hot" });
+
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
         <Footer />
       </div>
     </div>

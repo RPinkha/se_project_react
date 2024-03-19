b2ebc9227e9003ea282e2b36dc8aa4e6fe596b0f
RuvenPinkhasov <rubi00010@gmail.com>
2024-03-18 20:35:16 -0400

    finished Footer design and outline

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import "./Footer.css";
 
 function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <p className="footer__developer">Developed by Ruven Pinkhasov</p>
+      <p className="footer__year">2024</p>
+    </footer>
+  );
 }
 
 export default Footer;

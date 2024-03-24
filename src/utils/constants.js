6e3a951a6b0a68ebce32c7ff35921c3863d66455
RuvenPinkhasov <rubi00010@gmail.com>
2024-03-24 02:56:53 -0400

    feat: added changing weather card based on weather

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -48,5 +48,61 @@ export const weatherOptions = [
   {
     day: true,
     condition: "clear",
+    url: new URL("../assets/day/clear.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../assets/day/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "foggy",
+    url: new URL("../assets/day/foggy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "rainy",
+    url: new URL("../assets/day/rainy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snowy",
+    url: new URL("../assets/day/snowy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "stormy",
+    url: new URL("../assets/day/stormy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../assets/night/clear.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../assets/night/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "foggy",
+    url: new URL("../assets/night/foggy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rainy",
+    url: new URL("../assets/night/rainy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snowy",
+    url: new URL("../assets/night/snowy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "stormy",
+    url: new URL("../assets/night/stormy.png", import.meta.url).href,
   },
 ];

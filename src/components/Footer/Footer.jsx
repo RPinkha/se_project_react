8ee078f6faad1db1eb18fe13c610632a1d5feff4
RuvenPinkhasov <rubi00010@gmail.com>
2024-03-24 11:30:11 -0400

    fix: thee year in the footer is achieved with a function

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__developer">Developed by Ruven Pinkhasov</p>
-      <p className="footer__year">2024</p>
+      <p className="footer__year">{new Date().getFullYear()}</p>
     </footer>
   );
 }

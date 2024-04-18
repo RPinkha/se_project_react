3e45f42806c98edd54a97cdf6522ec3e2331cfaa
RuvenPinkhasov <rubi00010@gmail.com>
2024-04-17 22:44:02 -0400

    feat: created toggleswitch files and started writing jsx for it

---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,11 @@
+import "./ToggleSwitch.css";
+
+function ToggleSwitch() {
+  return (
+    <div className="toggle-switch">
+      <input type="checkbox" className="toggle-switch__box" />
+    </div>
+  );
+}
+
+export default ToggleSwitch;

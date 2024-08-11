4ae5a79d39215fabbfa344542cf3d72b9c76f3ec
RuvenPinkhasov <rubi00010@gmail.com>
2024-08-11 13:35:42 -0400

    feat: created user authorization file, auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,24 @@
+import { baseUrl } from "./constants";
+import { checkResponse } from "./utils";
+
+export const register = (email, password, name, avatar) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  }).then(checkResponse);
+};
+
+export const authorize = (email, password) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};

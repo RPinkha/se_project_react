5d62c5b2eff27e75cf4f2d41a1a293c0827a1079
RuvenPinkhasov <rubi00010@gmail.com>
2024-08-11 13:35:05 -0400

    feat: created LoginModal

---
+++ src/components/LoginModal/LoginModal.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import validator from "validator";
+
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+
+function LoginModal({ handleCloseClick, isOpen, onLoginSubmit }) {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(true);
+
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLoginSubmit({ email, password });
+  };
+
+  const handleEmailChange = (e) => {
+    const newValue = e.target.value;
+    setEmail(newValue);
+    const isValid = validateEmail(newValue);
+    setEmailError(isValid);
+  };
+
+  const validateEmail = (input) => {
+    return validator.isEmail(input);
+  };
+
+  const handlePasswordChange = (e) => {
+    const newValue = e.target.value;
+    setPassword(newValue);
+    const isValid = validatePassword(newValue);
+    setPasswordError(isValid);
+  };
+
+  const validatePassword = (input) => {
+    return validator.isLength(input, { min: 6 });
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setEmail("");
+      setPassword("");
+    }
+  }, [isOpen]);
+
+  return (
+    <ModalWithForm
+      formTitle="Log In"
+      handleCloseClick={handleCloseClick}
+      isOpen={isOpen}
+      onSubmit={handleSubmit}
+    >
+      <label
+        htmlFor="Email"
+        className={`modal__label${
+          emailError ? "" : " modal__label_type_error"
+        }`}
+      >
+        Email
+        <input
+          type="text"
+          className={`modal__input${
+            emailError ? "" : " modal__input_type_error"
+          }`}
+          id="Email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </label>
+      <label
+        htmlFor="Password"
+        className={`modal__label${
+          passwordError ? "" : " modal__label_type_error"
+        }`}
+      >
+        Password
+        <input
+          type="url"
+          className={`modal__input${
+            passwordError ? "" : " modal__input_type_error"
+          }`}
+          id="Password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      </label>
+      <div className="modal__buttons">
+        <button type="submit" className="modal__submit">
+          Log In
+        </button>
+        <button type="button" className="modal__second-option">
+          or Sign Up
+        </button>
+      </div>
+    </ModalWithForm>
+  );
+}
+
+export default LoginModal;

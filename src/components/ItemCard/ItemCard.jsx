959cd3ba958645ffd0aa3f55e69ca2999db3a55b
RuvenPinkhasov <rubi00010@gmail.com>
2024-08-13 16:33:34 -0400

    feat: added like button and styled it

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,6 +1,12 @@
 import "./ItemCard.css";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../context/CurrentUserContext";
 
 function ItemCard({ item, onCardClick }) {
+  const { userData } = useContext(CurrentUserContext);
+
+  const isOwn = item.owner === userData._id;
+
   return (
     <li
       className="card"
@@ -8,7 +14,13 @@ function ItemCard({ item, onCardClick }) {
         onCardClick(item);
       }}
     >
-      <h2 className="card__title">{item.name}</h2>
+      <div className="card__heading">
+        <h2 className="card__title">{item.name}</h2>
+        <button
+          className="card__like-button"
+          //.card__like-button_active
+        ></button>
+      </div>
       <img src={item.imageUrl} alt={item.name} className="card__image" />
     </li>
   );
